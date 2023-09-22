09dd1d78aa2f32ad2a65584f01421f1428eb3e10
bumm1y <becheverria@usm.cl>
2023-09-21 21:06:24 -0300

    --> Conexión BD <--

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Abecedario, Categoria, Explicaciones, Actividades
 
-# Register your models here.
+''' Credenciales user admin'''
+# Username: admin
+# Passwd: admin
+
+''' Vista de tablas mediante el user admin (Se visualiza en la página de Django)'''
+
+admin.site.register(Abecedario)
+admin.site.register(Categoria)
+admin.site.register(Explicaciones)
+admin.site.register(Actividades)
+
+
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+#Creación de tablas en bbdd.
+
+''' Tablas tipo contenido '''
+# --> Verificar largo de cada str
+
+class Categoria(models.Model):
+    nom_categoria = models.CharField(max_length=20)    # <-- Buena práctica del uso de ForeignKey?
+
+    def __str__(self):
+        return self.nom_categoria
+    
+class Abecedario(models.Model):
+    idabc = models.AutoField(primary_key=True)
+    letra = models.CharField(max_length=1)
+    linkimg = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.idabc}, {self.letra}, {self.linkimg}"
+
+
+''' Tabla Explicaciones'''
+class Explicaciones(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    img = models.CharField(max_length=500)
+    vid = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.categoria}, {self.img}, {self.vid}"
+
+''' Tabla Actividades '''
+class Actividades(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    videos = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.categoria}, {self.videos}"
+
+
